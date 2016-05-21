d9aa7b439966a43a623850bec7953c6198b49491
HARADA REO <rharada@golyat.local>
2016-05-22 03:32:09 +0900

    second commit

--- my辞書/DetailViewController.swift
+++ my辞書/DetailViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var detailWebView: UIWebView!
+    var query: String!
+    var type: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        var urlString = "https://ja.wikipedia.org/wiki/\(self.query)"
+        
+        if(self.type == "google_img"){
+            urlString = "https://www.google.co.jp/search?q=\(self.query)&es_sm=91&prmd=iv&source=lnms&tbm=isch&sa=X&ved=0CAcQ_AUoAWoVChMI2ZKO5rTXxwIVjJCUCh1F3Q4r&biw=414&bih=736&dpr=3"
+        }
+        
+        let url = NSURL(string: urlString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!)
+        let request = NSURLRequest(URL: url!)
+        self.detailWebView.loadRequest(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
